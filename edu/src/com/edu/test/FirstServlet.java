b237908ae20ac70a8d5d9ba82336fc9586b27dd4
GunukLyoo <gunuk418@gmail.com>
2022-03-28 20:30:53 +0900

    practice about serlvet get method

---
+++ edu/src/com/edu/test/FirstServlet.java
@@ -0,0 +1,24 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello2")
+public class FirstServlet extends HttpServlet {
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init() ½ÇÇàµÊ!");
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("service() ½ÇÇàµÊ!");
+	}
+	
+	
+}
---
+++ edu/src/com/edu/test/HeaderInfoServlet.java
@@ -0,0 +1,30 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.util.*;
+
+@WebServlet("/headerInfo")
+public class HeaderInfoServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out = resp.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력 Servlet</title></head>");
+		out.print("<body>");
+		out.print("<h3>요청 헤더 정보</h3>");
+		Enumeration<String> em = req.getHeaderNames();
+		while(em.hasMoreElements()) {
+			String s = em.nextElement();
+			out.println(s + " : " + req.getHeader(s) + "<br/>");
+		}
+		out.print("</body></html>");
+		out.close();
+	}
+	
+}

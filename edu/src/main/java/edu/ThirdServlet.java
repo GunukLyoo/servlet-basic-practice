59099e1e2f49ac087a284bc3bd16bc5100fd9544
GunukLyoo <gunuk418@gmail.com>
2022-04-05 00:51:49 +0900

    server error resolved

---
+++ edu/src/main/java/edu/ThirdServlet.java
@@ -0,0 +1,20 @@
+package edu;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>좋은 하루!</h1>");
+		out.close();
+	}
+	
+}

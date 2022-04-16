4af7baf2e3360a9a9c32ebbdd0a7cdede7fe2d8d
GunukLyoo <gunuk418@gmail.com>
2022-04-16 22:27:10 +0900

    init to httpservlet

--- edu/src/main/java/edu/ServletContextTest1Servlet.java
+++ edu/src/main/java/edu/ServletContextTest1Servlet.java
@@ -8,19 +8,20 @@ import javax.servlet.http.*;
 @WebServlet("/context1")
 public class ServletContextTest1Servlet extends HttpServlet{
 	
-	ServletContext sc;
+//	ServletContext sc;
 	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
+//	@Override
+//	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
-		sc = config.getServletContext();
-	}
+		//sc = config.getServletContext();
+//	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
+		ServletContext sc = this.getServletContext();
 		out.print("Context : " + sc);
 		out.close();
 	}	

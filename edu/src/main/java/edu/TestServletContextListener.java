ad0be4f13b667555175de78af76fbe430fe8b7c6
GunukLyoo <gunuk418@gmail.com>
2022-05-03 23:16:26 +0900

    servlet context

---
+++ edu/src/main/java/edu/TestServletContextListener.java
@@ -0,0 +1,24 @@
+package edu;
+
+import javax.servlet.*;
+
+public class TestServletContextListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		System.out.println("ServletContext 按眉 秦力");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		System.out.println("ServletContext 按眉 檬扁拳");
+	}
+
+	public TestServletContextListener() {
+		// TODO Auto-generated constructor stub
+		System.out.println("TestServletContextListener 按眉 积己");
+	}
+
+}
